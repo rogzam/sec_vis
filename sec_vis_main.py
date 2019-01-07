1c00e2a5efc747294c4d966b4da0b2e1cbcc1c8f
rogzam <rogzam@gmail.com>
2019-01-07 19:02:56 +0100

    Back to a main file, let's leave functions on a different file for later

--- sec_vis_main.py
+++ sec_vis_main.py
@@ -1,6 +1,38 @@
 import rhinoscriptsyntax as rs
-from sec_vis_func import Mod_siz
 
-print('The model is: {}(w) * {}(d) * {}(h).'.format(Mod_siz()[0],Mod_siz()[1],Mod_siz()[2]))
+count = 0
+#Select all visible objects
+obj_all = rs.AllObjects(select=True)
 
-print(Mod_siz()[5])
+#Change their name and create a list of the points for the object bounding box
+rs.ObjectName(obj_all,'obj_main')
+obj_box = rs.BoundingBox(obj_all)
+
+#Print coordinades with tags for visualization purposes
+#for i, point in enumerate(obj_box)
+#    rs.AddTextDot(i,point)
+
+#Grab the relevant coordinades from the list only and name them O, X , Y and Z
+crd_rel = []
+ind_rel = [0,1,3,4]
+
+for i in ind_rel:
+    crd_rel.append(obj_box[i])
+    #pnt = rs.AddPoint(obj_box[i])
+    if i == 0:
+        crd_o = obj_box[i]
+    if i == 1:
+        crd_x = obj_box[i]
+    if i == 3:
+        crd_y = obj_box[i]
+    if i == 4:
+        crd_z = obj_box[i]
+    else:
+        pass
+
+#Calculate the height, width and depth of the model
+obj_hei = round(rs.Distance(crd_o,crd_z),2)
+obj_wid = round(rs.Distance(crd_o,crd_x),2)
+obj_dep = round(rs.Distance(crd_o,crd_y),2)
+
+#print('The model is: {}(w) * {}(d) * {}(h).'.format(Mod_siz()[0],Mod_siz()[1],Mod_siz()[2]))
