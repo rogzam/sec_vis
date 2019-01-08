62118d23f1cba83a17753cf14faa07fc01a6638d
rogzam <rogzam@gmail.com>
2019-01-08 20:32:40 +0100

    Tested the production of section curves in a different script.

--- sec_vis_main.py
+++ sec_vis_main.py
@@ -2,11 +2,10 @@ import rhinoscriptsyntax as rs
 
 #Define the image paremeters
 img_zoo = 5
-img_fol = '/Users/Rog/Desktop'
-img_nam = '/img_01'
+img_fol = '/Users/Rog/Desktop/test'
+img_pfx = '/img_'
 img_wid = 1200
 img_hei = 600
-img_des = img_fol + img_nam + '.png'
 
 #Select all visible objects
 obj_all = rs.AllObjects(select=True)
@@ -50,31 +49,28 @@ print('The model is: {}(width) * {}(depth) * {}(height)'.format(obj_wid,obj_hei,
 
 #Ask for the resolution of the section view animation
 #vis_res = rs.GetInteger(message='Please insert the resolution for the animation (number of slices)',number=10,minimum=2,maximum=100)
-vis_res = 10
+vis_res = 15
 
 # #Calculate the step value to move the plane
-pla_org = crd_o
+pla_org = -crd_o
 vis_ste = obj_dep/vis_res
+pla_org[1] = pla_org[1] + (vis_ste/2)
 pla_pos = pla_org
 
-
-#Main loop taking the shots on each iteration
-for i in range(vis_res):
-    pla_obj = rs.AddClippingPlane(rs.WorldZXPlane(),50,50,views='Perspective')
-    rs.MoveObject(pla_obj,pla_pos)
-    #rs.AddPoint(pla_pos)
-    pla_pos[1] = pla_pos[1] + vis_ste
-    rs.DeleteObject(pla_obj)
-
-#Testing the screenshot bit
-
 #Center the objects with respect to the camera
-rs.Command('-_Perspective')
+#rs.Command('-_Perspective')
 rs.SelectObjects(obj_all)
 rs.ZoomSelected()
 rs.UnselectAllObjects()
 for i in range(img_zoo):
     rs.Command('Zoom Out')
 
-#Produce the image
-rs.Command('-_ViewCaptureToFile ' + img_des + ' _DrawGrid=No' + ' _Width=' + str(img_wid) + ' _Height=' + str(img_hei) + ' _TransparentBackground=Yes' + ' _Enter' + ' _Enter')
+#Main loop taking the shots on each iteration
+for i in range(vis_res+2):
+    pla_obj = rs.AddClippingPlane(rs.WorldZXPlane(),50,50,views='Perspective')
+    rs.MoveObject(pla_obj,pla_pos)
+    #rs.AddPoint(pla_pos)
+    img_des = img_fol + img_pfx + str(i) + '.png'
+    rs.Command('-_ViewCaptureToFile ' + img_des + ' _DrawGrid=No' + ' _Width=' + str(img_wid) + ' _Height=' + str(img_hei) + ' _TransparentBackground=Yes' + ' _Enter' + ' _Enter')
+    pla_pos[1] = pla_pos[1] - vis_ste
+    rs.DeleteObject(pla_obj)
